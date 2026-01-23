e9f2f8916bb8ec69302409d5adf50778fa323ae9
HemanthRaj0C <personalhemanthraj@gmail.com>
2026-01-24 00:27:50 +0530

    [hush] layLow

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA01AdAXUKScvUjq5WNF6Yw6CugM1Q9GNY",
-  authDomain: "sriram-soc.firebaseapp.com",
-  projectId: "sriram-soc",
-  storageBucket: "sriram-soc.firebasestorage.app",
-  messagingSenderId: "72428991090",
-  appId: "1:72428991090:web:e8aefe526dfa7dcad488eb",
-  measurementId: "G-L6HEMDYPN1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
